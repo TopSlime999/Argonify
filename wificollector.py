4f1535760a0bb55aac87c46da40c3f263e5aeb0f
Malek Kamal <120230397+El-Qassam@users.noreply.github.com>
2023-03-25 10:33:11 -0400

    Update wificollector.py

--- wificollector.py
+++ wificollector.py
@@ -6,4 +6,6 @@ pchostname=socket.gethostname() # <--- Finds PC Hostname
 ipaddress=socket.gethostbyname(pchostname) # <--- Finds IP Address
 print("Your Computer Hostname is: "+pchostname) # <--- Says Hostname
 print("Your IP Address is: "+ipaddress) # <--- Says IP 
-
+text_file = open("wifi.txt", "w") # <--- Creates a file to write info in
+n = text_file.write('PC Hostname: '+str(pchostname)+' IP Address: '+str(ipaddress)) # <--- Writes info in a file
+text_file.close() # <--- Closes the file
